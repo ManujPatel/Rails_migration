6babc17f57202041abd650b8b3eb159d6542c799
Manuj Patel <manuj.p@simformsolutions.com>
2022-01-26 23:49:42 +0530

    Create author and genrate controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'authors/index'
+  get 'authors/new'
+  get 'authors/create'
+  get 'authors/show'
+  get 'authors/edit'
+  get 'authors/update'
+  get 'authors/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
